a1da3eba275ec30c4854f2f5526ddcf191c53bcb
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-04-11 13:32:51 +0300

    Первый коммит

--- src/First/Main.java
+++ src/First/Main.java
@@ -1,7 +1,8 @@
-package com.yura.idea;
+package First;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World!");
     }
 }
+
