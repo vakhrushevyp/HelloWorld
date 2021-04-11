25be000cf1d1fbbe408efc3916bba671c4e2ad4e
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-04-11 15:07:54 +0300

    Второй коммит

---
+++ src/Second/MainSecond.java
@@ -0,0 +1,10 @@
+package Second;
+
+public class MainSecond {
+    public static void main(String[] args) {
+        double dub = 11.04;
+        byte by = 127;
+        System.out.println(dub);
+        System.out.println(by);
+    }
+}
