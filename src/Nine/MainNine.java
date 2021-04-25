4260cea24b1d25b654fad1d6455cf465ed4357ef
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-04-25 12:20:37 +0300

    Девятый коммит

---
+++ src/Nine/MainNine.java
@@ -0,0 +1,21 @@
+package Nine;
+import java.util.Scanner;
+public class MainNine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sr = 0;
+        System.out.println("Введите размер массива:");
+        int i = scanner.nextInt();
+        double[] myArray = new double[i];
+        System.out.println("Введите элементы массива в количестве " + i + " чисел:");
+        for (int j = 0; j < myArray.length; j++) {
+            myArray[j] = scanner.nextInt();
+            sr += myArray[j];
+        }
+        sr = sr / i;
+        for (double outArray : myArray) {
+            System.out.print(outArray * sr + " ");
+        }
+    }
+
+}
