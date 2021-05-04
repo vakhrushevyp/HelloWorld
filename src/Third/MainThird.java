58f70494f7db97701e45d148c15e01d68762fffa
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-05-04 08:22:39 +0300

    Третий исправлен

--- src/Third/MainThird.java
+++ src/Third/MainThird.java
@@ -4,8 +4,8 @@ public class MainThird {
     public static void main(String[] args) {
         int[] myArray = {1,50,25,45,2};
         int temp  = myArray[0];
-        myArray[0] = myArray[4];
-        myArray[4] = temp;
-        System.out.println(myArray[0]+myArray[2]);
+        myArray[0] = myArray[myArray.length - 1];
+        myArray[myArray.length - 1] = temp;
+        System.out.println(myArray[0]+myArray[myArray.length/2]);
     }
 }
