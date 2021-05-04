76df151eb16fb24dbac2c43d565bc383cb440063
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-05-04 09:23:55 +0300

    Десятый исправлен: (изменил i и j местами при вводе размера массива.

--- src/Ten/MainTen.java
+++ src/Ten/MainTen.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class MainTen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер матрицы, кол-во срок:");
-        int i = scanner.nextInt();
-        System.out.println("Введите размер матрицы, кол-во столбцов:");
+        System.out.println("Введите размер матрицы, кол-во строк:");
         int j = scanner.nextInt();
+        System.out.println("Введите размер матрицы, кол-во столбцов:");
+        int i = scanner.nextInt();
         int[][] myArray = new int[j][i];
-        System.out.println("Заполните матрицу " + i + " на " + j + " числами:");
+        System.out.println("Заполните матрицу " + j + " на " + i + " числами:");
         for (int x = 0; x < j; x++ ) {
             for (int y = 0; y < i; y++ ) {
                 myArray[x][y] = scanner.nextInt();
