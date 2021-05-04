9fbbbfb4163f1276b48af740b1287a9aa249c87c
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-05-04 08:49:26 +0300

    Четвертый исправлен

--- src/Four/MainFour.java
+++ src/Four/MainFour.java
@@ -4,12 +4,13 @@ public class MainFour {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Input x,y,z: ");
-        int x = in.nextInt();
-        int y = in.nextInt();
-        int z = in.nextInt();
-        int sr = (x+y+z)/3;
-        System.out.println(sr);
-        if (sr/2 > 3) {
+        float x = in.nextFloat();
+        float y = in.nextFloat();
+        float z = in.nextFloat();
+        float sr = (x+y+z)/3;
+        System.out.println("Среднее арифметическое: " + sr);
+        int okr = (int) (sr/2);
+        if (okr > 3) {
             System.out.println("Программа выполнена корректно");
         }
     }
