185c819e82667dcc6cd503f545e26680e4707b7c
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2021-05-14 20:53:18 +0300

    Итоговое 3. Сократил код

--- src/Final3/MainFinal3.java
+++ src/Final3/MainFinal3.java
@@ -7,16 +7,12 @@ public class MainFinal3 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Количество строк: ");
         int n = scanner.nextInt();
-        int s = 1;
-        int i1;
         String[] myArray = new String[n];
         int[] myArray2 = new int[n];
         for (int i = 0; i < n; i++) {
-            System.out.print("Строка " + s + ": ");
+            System.out.print("Строка " + (i+1) + ": ");
             myArray[i] = scanner.next();
-            s++;
-            myArray2[i] = 0;
-            for (i1 = 0; i1 < myArray[i].length(); i1++) {
+            for (int i1 = 0; i1 < myArray[i].length(); i1++) {
                 if (myArray[i].indexOf(myArray[i].charAt(i1), i1 + 1) == -1) {
                     myArray2[i]++;
                 }
